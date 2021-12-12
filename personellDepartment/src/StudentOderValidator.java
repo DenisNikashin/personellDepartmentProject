06d53123bd92630d6773f0811298cb95bc9ee285
denis <denis0n@ukr.net>
2021-12-12 21:40:22 +0200

    Non-standard operation (issue 5). Study Java (Anton Saburov)

---
+++ personellDepartment/src/StudentOderValidator.java
@@ -0,0 +1,23 @@
+public class StudentOderValidator {
+    public static void main(String[] args) {
+        checkAll();
+    }
+    static void checkAll() {
+        checkCityRegister();
+        checkWedding();
+        checkChildren();
+        checkStudents();
+    }
+    static void checkCityRegister() {
+        System.out.println("CityRegister is running");
+    }
+    static void checkWedding() {
+        System.out.println("Wedding is running");
+    }
+    static void checkChildren() {
+        System.out.println("Check Children method is running");
+    }
+    static void checkStudents() {
+        System.out.println("Students is checking");
+    }
+}
